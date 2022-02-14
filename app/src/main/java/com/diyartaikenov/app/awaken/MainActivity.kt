3b829fc03188227625ac29052e52209522a6040a
ksalarang <anbleidd@gmail.com>
2022-02-15 03:15:29 +0600

    Set up MainActivity onCreate(), added binding field

--- app/src/main/java/com/diyartaikenov/app/awaken/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/MainActivity.kt
@@ -2,10 +2,30 @@ package com.diyartaikenov.app.awaken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.diyartaikenov.app.awaken.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.nav_home, R.id.nav_stats, R.id.nav_settings)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
