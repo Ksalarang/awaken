02b50587cc87ff7bac2b37283bdd6984af9bc607
ksalarang <anbleidd@gmail.com>
2022-02-16 03:11:22 +0600

    Added MeditationPresetDatabase

---
+++ app/src/main/java/com/diyartaikenov/app/awaken/data/MeditationPresetDatabase.kt
@@ -0,0 +1,36 @@
+package com.diyartaikenov.app.awaken.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.diyartaikenov.app.awaken.model.MeditationPreset
+
+/**
+ * A Room database to persist data for the Awaken app.
+ * This database stores a [MeditationPreset] entity.
+ */
+@Database(entities = [MeditationPreset::class], version = 1)
+abstract class MeditationPresetDatabase: RoomDatabase() {
+    abstract fun meditationPresetDao(): MeditationPresetDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MeditationPresetDatabase? = null
+
+        fun getDatabase(context: Context): MeditationPresetDatabase {
+            return INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    MeditationPresetDatabase::class.java,
+                    "meditation_preset_db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
