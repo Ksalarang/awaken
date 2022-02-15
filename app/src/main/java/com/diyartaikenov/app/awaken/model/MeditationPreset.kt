28fc6a54dda00ed8f5af454fb8f5a5ddb2eba754
ksalarang <anbleidd@gmail.com>
2022-02-16 01:50:38 +0600

    Added Room annotations for MeditationPreset entity

--- app/src/main/java/com/diyartaikenov/app/awaken/model/MeditationPreset.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/model/MeditationPreset.kt
@@ -1,11 +1,18 @@
 package com.diyartaikenov.app.awaken.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Meditation Preset entity to be stored in meditation_preset_db.
  */
 
+@Entity(tableName = "meditation_preset_db")
 data class MeditationPreset(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val name: String,
+    @ColumnInfo(name = "duration_in_minutes")
     val durationInMinutes: Int
 )
