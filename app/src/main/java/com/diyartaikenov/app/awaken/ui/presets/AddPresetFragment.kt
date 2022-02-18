203d0bfb4efc8585d64066f5ae8c0f159a5bed79
ksalarang <anbleidd@gmail.com>
2022-02-19 03:42:08 +0600

    Added AddPresetFragment and its layout file

---
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/AddPresetFragment.kt
@@ -0,0 +1,13 @@
+package com.diyartaikenov.app.awaken.ui.presets
+
+import androidx.fragment.app.Fragment
+import com.diyartaikenov.app.awaken.R
+import com.diyartaikenov.app.awaken.model.MeditationPreset
+
+/**
+ * A [Fragment] to enter data for a new [MeditationPreset] or to edit data for
+ * an existing [MeditationPreset].
+ */
+class AddPresetFragment : Fragment(R.layout.fragment_add_preset) {
+    
+}
