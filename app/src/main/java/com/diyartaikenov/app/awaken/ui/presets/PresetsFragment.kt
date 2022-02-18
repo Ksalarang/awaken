59eeb4bbc0a8365735809a51d0fc96c02acc9e41
ksalarang <anbleidd@gmail.com>
2022-02-19 03:31:43 +0600

    Added doc comment to the PresetsFragment class and a viewModel field

--- app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/PresetsFragment.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/PresetsFragment.kt
@@ -1,8 +1,21 @@
 package com.diyartaikenov.app.awaken.ui.presets
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.diyartaikenov.app.awaken.BaseApplication
 import com.diyartaikenov.app.awaken.R
+import com.diyartaikenov.app.awaken.model.MeditationPreset
+import com.diyartaikenov.app.awaken.ui.viewmodel.PresetViewModel
+import com.diyartaikenov.app.awaken.ui.viewmodel.PresetViewModelFactory
 
-class PresetsFragment : Fragment(R.layout.fragment_presets) {
+/**
+ * A [Fragment] to view the list of [MeditationPreset]s stored in the database.
+ */
+class PresetsFragment: Fragment(R.layout.fragment_presets) {
 
+    private val viewModel: PresetViewModel by activityViewModels {
+        PresetViewModelFactory(
+            (activity?.application as BaseApplication).database.meditationPresetDao()
+        )
+    }
 }
