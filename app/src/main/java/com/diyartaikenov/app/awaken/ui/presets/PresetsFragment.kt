ffaf548b18bc26feefebee0b730a614cb86f1893
ksalarang <anbleidd@gmail.com>
2022-02-20 05:41:41 +0600

    Set up onCreateView() and onViewCreated() in PresetsFragment.kt

--- app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/PresetsFragment.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/PresetsFragment.kt
@@ -1,9 +1,15 @@
 package com.diyartaikenov.app.awaken.ui.presets
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.diyartaikenov.app.awaken.BaseApplication
 import com.diyartaikenov.app.awaken.R
+import com.diyartaikenov.app.awaken.databinding.FragmentPresetsBinding
 import com.diyartaikenov.app.awaken.model.MeditationPreset
 import com.diyartaikenov.app.awaken.ui.viewmodel.PresetViewModel
 import com.diyartaikenov.app.awaken.ui.viewmodel.PresetViewModelFactory
@@ -11,11 +17,32 @@ import com.diyartaikenov.app.awaken.ui.viewmodel.PresetViewModelFactory
 /**
  * A [Fragment] to view the list of [MeditationPreset]s stored in the database.
  */
-class PresetsFragment: Fragment(R.layout.fragment_presets) {
+class PresetsFragment: Fragment() {
 
     private val viewModel: PresetViewModel by activityViewModels {
         PresetViewModelFactory(
             (activity?.application as BaseApplication).database.meditationPresetDao()
         )
+    }
+
+    private lateinit var binding: FragmentPresetsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPresetsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddMeditationPreset.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_nav_presets_to_nav_add_preset_fragment
+            )
+        }
     }
 }
