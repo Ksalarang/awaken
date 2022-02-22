7195025b62428a33f32d4336fcbad89babe2fe22
ksalarang <anbleidd@gmail.com>
2022-02-22 06:26:40 +0600

    Add addPreset() method to add preset to the database

--- app/src/main/java/com/diyartaikenov/app/awaken/ui/viewmodel/PresetViewModel.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/viewmodel/PresetViewModel.kt
@@ -3,8 +3,11 @@ package com.diyartaikenov.app.awaken.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.diyartaikenov.app.awaken.data.MeditationPresetDao
 import com.diyartaikenov.app.awaken.model.MeditationPreset
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Shared [ViewModel] to provide data to the [PresetsFragment]
@@ -14,4 +17,11 @@ class PresetViewModel(private val presetDao: MeditationPresetDao): ViewModel() {
 
     val presets: LiveData<List<MeditationPreset>> =
         presetDao.getMeditationPresets().asLiveData()
+
+    fun addPreset(name: String, duration: Int) {
+        val preset = MeditationPreset(name = name, durationInMinutes = duration)
+        viewModelScope.launch(Dispatchers.IO) {
+            presetDao.insert(preset)
+        }
+    }
 }
