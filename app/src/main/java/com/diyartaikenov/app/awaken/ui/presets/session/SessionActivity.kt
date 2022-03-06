3d6cb1c581dbf4f98d4b779a6768d8e6c336ce69
ksalarang <anbleidd@gmail.com>
2022-03-07 04:52:05 +0600

    refactor: move the hideSystemBars method to a new utils package

--- app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/session/SessionActivity.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/session/SessionActivity.kt
@@ -2,11 +2,9 @@ package com.diyartaikenov.app.awaken.ui.presets.session
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.navArgs
 import com.diyartaikenov.app.awaken.databinding.ActivitySessionBinding
+import com.diyartaikenov.app.awaken.utils.Utils
 
 class SessionActivity : AppCompatActivity() {
 
@@ -20,19 +18,9 @@ class SessionActivity : AppCompatActivity() {
         binding = ActivitySessionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        hideSystemBars()
+        Utils.hideSystemBars(window.decorView)
         supportActionBar?.hide()
 
         binding.etMinutes.text = navArgs.duration.toString()
-    }
-
-    private fun hideSystemBars() {
-        val windowInsetsController =
-            ViewCompat.getWindowInsetsController(window.decorView) ?: return
-
-        windowInsetsController.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-
-        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
---
+++ app/src/main/java/com/diyartaikenov/app/awaken/utils/Utils.kt
@@ -0,0 +1,24 @@
+package com.diyartaikenov.app.awaken.utils
+
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class Utils {
+    companion object {
+        /**
+         * Hide the status bar and the navigation bar.
+         * Also change the system bars' behavior to show transient bars by swipe.
+         */
+        fun hideSystemBars(windowDecorView: View) {
+            val windowInsetsController =
+                ViewCompat.getWindowInsetsController(windowDecorView) ?: return
+
+            windowInsetsController.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+        }
+    }
+}
