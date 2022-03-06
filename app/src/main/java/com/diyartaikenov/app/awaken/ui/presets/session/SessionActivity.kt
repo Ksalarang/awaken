da252450fa9f1d1414be2a1f756e77e6708b8c69
ksalarang <anbleidd@gmail.com>
2022-03-07 01:01:24 +0600

    add: Session activity with its layout

---
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/session/SessionActivity.kt
@@ -0,0 +1,12 @@
+package com.diyartaikenov.app.awaken.ui.presets.session
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diyartaikenov.app.awaken.R
+
+class SessionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_session)
+    }
+}
