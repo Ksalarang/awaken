8939827f7e1f39eec1b402c920cae0e76808fb48
ksalarang <anbleidd@gmail.com>
2022-03-10 04:55:47 +0600

    chore: add newline to end of files

--- app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/session/SessionActivity.kt
+++ app/src/main/java/com/diyartaikenov/app/awaken/ui/presets/session/SessionActivity.kt
@@ -18,10 +18,9 @@ import com.diyartaikenov.app.awaken.utils.Utils
 
 class SessionActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySessionBinding
-
     private val navArgs: SessionActivityArgs by navArgs()
 
+    private lateinit var binding: ActivitySessionBinding
     private lateinit var broadcastManager: LocalBroadcastManager
     private lateinit var sessionStateReceiver: BroadcastReceiver
 
